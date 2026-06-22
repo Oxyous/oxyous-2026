371baf6a11da8e86711f25f403b20da2cd6def43
oxyous <sjoebaldwin@outlook.com>
2026-06-22 13:32:31 +0100

    Ray Plane Collision

---
+++ app/src/main/cpp/engine/collision/Collision.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Mr Steven J Baldwin on 22/06/2026.
+//
+
+#ifndef OXYOUS_2026_COLLISION_HPP
+#define OXYOUS_2026_COLLISION_HPP
+
+#endif //OXYOUS_2026_COLLISION_HPP
---
+++ app/src/main/cpp/engine/collision/PlaneVolume.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Mr Steven J Baldwin on 22/06/2026.
+//
